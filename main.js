b3eee460d42f322a86bc09100e796a08d2a71e94
andreasavignano02 <112469730+andreasavignano02@users.noreply.github.com>
2022-09-26 15:01:28 +0200

    finito esercizio

--- main.js
+++ main.js
@@ -1,7 +1,18 @@
+// Variabili
 let ora = new Date();
-data.getMonth(); 
+ora.getHours();
+let gg, mm, aaaa;
+gg = ora.getDate() + "/";
+mm = ora.getMonth() + 1 + "/";
+aaaa = ora.getFullYear();
 
-function oratotale() {
+// Funzioni
+function oraTotale() {
     document.getElementById("visione").innerHTML = ora
 }
-
+function vediOra(){
+document.getElementById("ora").innerHTML = ora.getHours()
+}
+function visioneFormato() {
+    document.getElementById("formatoEur").innerHTML = ("Oggi è il "+ gg + mm + aaaa)
+}
